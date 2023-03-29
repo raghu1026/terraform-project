ddd1a0629d3a1deac7520a2fe94866335ac3d32b
raghu1026 <123620701+raghu1026@users.noreply.github.com>
2023-03-29 23:50:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 #this file consists of code for instances and sg
 provider "aws" {
-region = "ap-south-1"
-access_key = "AKIAXMY6O2REYJXNBYQW"
-secret_key = "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"
+  region     = "us-east-1"
+  access_key = "AKIAXMY6O2REYJXNBYQW"
+  secret_key = "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"
 }
 
 resource "aws_instance" "one" {
-  ami             = "ami-04581fbf744a7d11f"
-  instance_type   = "t2.micro"
-  key_name        = "jenkins"
+  ami                    = "ami-04581fbf744a7d11f"
+  instance_type          = "t2.micro"
+  key_name               = "jenkins"
   vpc_security_group_ids = [aws_security_group.three.id]
-  availability_zone = "us-east-1a"
-  user_data       = <<EOF
+  availability_zone      = "us-east-1a"
+  user_data              = <<EOF
 #!/bin/bash
 sudo -i
 yum install httpd -y
@@ -20,17 +20,17 @@ chkconfig httpd on
 echo "hai all this is my app created by terraform infrastructurte by raham sir server-1" > /var/www/html/index.html
 EOF
   tags = {
-    Name = "server-1"
+    Name = "server-11"
   }
 }
 
 resource "aws_instance" "two" {
-  ami             = "ami-04581fbf744a7d11f"
-  instance_type   = "t2.micro"
-  key_name        = "jenkins"
+  ami                    = "ami-04581fbf744a7d11f"
+  instance_type          = "t2.micro"
+  key_name               = "jenkins"
   vpc_security_group_ids = [aws_security_group.three.id]
-  availability_zone = "us-east-1b"
-  user_data       = <<EOF
+  availability_zone      = "us-east-1b"
+  user_data              = <<EOF
 #!/bin/bash
 sudo -i
 yum install httpd -y
@@ -39,12 +39,12 @@ chkconfig httpd on
 echo "hai all this is my website created by terraform infrastructurte by raham sir server-2" > /var/www/html/index.html
 EOF
   tags = {
-    Name = "server-2"
+    Name = "server-12"
   }
 }
 
 resource "aws_security_group" "three" {
-  name = "elb-sg"
+  name = "eeeelb-sg"
   ingress {
     from_port   = 22
     to_port     = 22
@@ -68,17 +68,17 @@ resource "aws_security_group" "three" {
 }
 
 resource "aws_s3_bucket" "four" {
-  bucket = "raghu2bucketterra"
+  bucket = "raaaghu2bucketterra"
 }
 
 resource "aws_iam_user" "five" {
-name = "rahamuser11" 
+  name = "rahamussse3r11"
 }
 
 resource "aws_ebs_volume" "six" {
- availability_zone = "ap-south-1b"
-  size = 40
+  availability_zone = "us-east-1b"
+  size              = 40
   tags = {
-    Name = "ebs-001"
+    Name = "ebs-0011"
   }
 }
